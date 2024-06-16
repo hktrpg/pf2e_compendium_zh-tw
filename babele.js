8cff14f227ba7d62545f705720e58b87f087787c
Sad <cougar.l@hotmail.com>
2024-06-16 15:18:16 +0800

    Update babele.js

--- babele.js
+++ babele.js
@@ -1,24 +1,24 @@
 Hooks.once("init", () => {
-  if (typeof Babele !== "undefined") {
-     game.babele.register({
-      module: "pf2e_compendium_zh-tw",
-      lang: "zh-tw",
-      dir: "compendium",
-    });
+    if (typeof Babele !== "undefined") {
+        game.babele.register({
+            module: "pf2e_compendium_zh-tw",
+            lang: "zh-tw",
+            dir: "compendium",
+        });
 
-    game.babele.registerConverters(({
-      "npc-portrait-path": (data, translations, dataObject, translatedCompendium, translationObject) => {
-        return game.npcTrans.portrait(data, translations, dataObject, translatedCompendium, translationObject);
-      },
-      "npc-token-translation": (data, translations, dataObject, translatedCompendium, translationObject) => {
-        return game.npcTrans.token(data, translations, dataObject, translatedCompendium, translationObject);
-      },
-      "npc-data-translation": (data, translations, dataObject, translatedCompendium, translationObject) => {
-        return game.npcTrans.data(data, translations, dataObject, translatedCompendium, translationObject);
-      },
-      "npc-item-translation": (data, translations, dataObject, translatedCompendium, translationObject) => {
-        return game.npcTrans.item(data, translations, dataObject, translatedCompendium, translationObject);
-      },
-    });
-  }
+        game.babele.registerConverters(({
+            "npc-portrait-path": (data, translations, dataObject, translatedCompendium, translationObject) => {
+                return game.npcTrans.portrait(data, translations, dataObject, translatedCompendium, translationObject);
+            },
+            "npc-token-translation": (data, translations, dataObject, translatedCompendium, translationObject) => {
+                return game.npcTrans.token(data, translations, dataObject, translatedCompendium, translationObject);
+            },
+            "npc-data-translation": (data, translations, dataObject, translatedCompendium, translationObject) => {
+                return game.npcTrans.data(data, translations, dataObject, translatedCompendium, translationObject);
+            },
+            "npc-item-translation": (data, translations, dataObject, translatedCompendium, translationObject) => {
+                return game.npcTrans.item(data, translations, dataObject, translatedCompendium, translationObject);
+            },
+        }));
+    }
 });
